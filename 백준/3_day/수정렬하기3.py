b75add0e0f3e7fd710a245c0cfa4b480c9b9ff3c
parkhongbeen <pack122@naver.com>
2020-06-29 17:04:51 +0900

    선택정렬

---
+++ 백준/3_day/수정렬하기3.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = [0] * 10001
+
+for i in range(n):
+    data = int(sys.stdin.readline())
+    arr[data] += 1
+
+for i in range(10001):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i)
